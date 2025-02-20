9cb6f6bf2a5c468b679debbff74375a68eacb1be
Yogesh Saini <121495438+2022BCD0052@users.noreply.github.com>
2025-02-20 14:58:25 +0530

    Refactor portfolio detail design layout and enhance tech stack display

--- src/app/data.js
+++ src/app/data.js
@@ -97,49 +97,79 @@ export const AboutData = [
 // this is for the slider 
 export const DataArray = [
   {
-    title: "Ultimate Nextjs13 Portfolio Website",
-    name: "Nextjs13 Portfolio Website",
-    des: "I have made this project in 2023  to show thousands of students all over the world  how to make Reactjs/Nextjs portfolio. A portfolio website that will enable you to showcase your projects to your client or your employer The design of this portfolio was taken from a friend. It was carefully and professionally design to work in all devices",
-    des1: "This project was made using, Ractjs, Nextjs, NodeMailer  and many more!",
-    images: ["/portfolioimage.png", "/nextportfolio1.png","/nextportfolio1.png","/nextportfolio1.png"],
+    "title": "FriendConnect Project",
+    "name": "FriendConnect ",
+    "des": "I developed this project in 2023 to guide students worldwide on how to build a portfolio using React.js and Next.js. This portfolio website allows developers to showcase their projects to clients or potential employers. The design was inspired by a friend’s concept and has been carefully crafted to be responsive across all devices.",
+    "des1": "This project was built using React.js, Next.js, NodeMailer, and other modern web technologies!",
+    "developer": "Yogesh Kumar Saini",
+    "techStack": ["React.js", "Next.js", "NodeMailer", "Tailwind CSS", "MongoDB"],
+    "images": [
+      "/projectImage/project1/friendconnect.png",
+      "/projectImage/project1/friendconnect2.png",
+      "/projectImage/project1/friendconnect3.png"
+    ],
+    "github": "https://github.com/2022BCD0052/Yogesh-Portfolio-1",  
+    "liveDemo": "https://yogesh-portfolio-1.vercel.app/"
   },
   {
-    title: "Personal Portfolio Website Using Reactjs",
-    name: "Ultimate ReactJs portfolio",
-    des: "I have made this project in 2022  to show thousands of students all over the world  how to make Reactjs portfolio.",
-    des1: "This project was made using, Ractjs, Nextjs, Expressjs, Nodejs, Rxjs Bootsrap and many more!",
-    images: ["/reactportfolio.png"],
+    "title": "FriendConnect Project",
+    "name": "FriendConnect ",
+    "des": "I developed this project in 2023 to guide students worldwide on how to build a portfolio using React.js and Next.js. This portfolio website allows developers to showcase their projects to clients or potential employers. The design was inspired by a friend’s concept and has been carefully crafted to be responsive across all devices.",
+    "des1": "This project was built using React.js, Next.js, NodeMailer, and other modern web technologies!",
+    "developer": "Yogesh Kumar Saini",
+    "techStack": ["React.js", "Next.js", "NodeMailer", "Tailwind CSS", "MongoDB"],
+    "images": [
+      "/projectImage/project1/friendconnect.png",
+      "/projectImage/project1/friendconnect2.png",
+      "/projectImage/project1/friendconnect3.png"
+    ],
+    "github": "https://github.com/2022BCD0052/Yogesh-Portfolio-1",  
+    "liveDemo": "https://yogesh-portfolio-1.vercel.app/"
   },
   {
-    title: "Management Consulting Company Web Design",
-    name: "edubaba Management Consultants",
-    des: "We buit this project for edubaba  Management Consultants.",
-    des1: "",
-
-    images: ["/hotel01.png", ],
+    "title": "FriendConnect Project",
+    "name": "FriendConnect ",
+    "des": "I developed this project in 2023 to guide students worldwide on how to build a portfolio using React.js and Next.js. This portfolio website allows developers to showcase their projects to clients or potential employers. The design was inspired by a friend’s concept and has been carefully crafted to be responsive across all devices.",
+    "des1": "This project was built using React.js, Next.js, NodeMailer, and other modern web technologies!",
+    "developer": "Yogesh Kumar Saini",
+    "techStack": ["React.js", "Next.js", "NodeMailer", "Tailwind CSS", "MongoDB"],
+    "images": [
+      "/projectImage/project1/friendconnect.png",
+      "/projectImage/project1/friendconnect2.png",
+      "/projectImage/project1/friendconnect3.png"
+    ],
+    "github": "https://github.com/2022BCD0052/Yogesh-Portfolio-1",  
+    "liveDemo": "https://yogesh-portfolio-1.vercel.app/"
   },
   {
-    title: "Real Estate and Property Management Project",
-    name: "Inans Property Management ",
-    des: "We built this project for inans group LTD",
-    des1: "Aside from its gorgeous design, this website is backed by an outstanding plugin that allows owners and agencies to simply display different properties in the neighbourhood and create an online showcase in an appealing manner for home buyers and tenants..",
-
-    images: ["/property1.png"],
+    "title": "FriendConnect Project",
+    "name": "FriendConnect ",
+    "des": "I developed this project in 2023 to guide students worldwide on how to build a portfolio using React.js and Next.js. This portfolio website allows developers to showcase their projects to clients or potential employers. The design was inspired by a friend’s concept and has been carefully crafted to be responsive across all devices.",
+    "des1": "This project was built using React.js, Next.js, NodeMailer, and other modern web technologies!",
+    "developer": "Yogesh Kumar Saini",
+    "techStack": ["React.js", "Next.js", "NodeMailer", "Tailwind CSS", "MongoDB"],
+    "images": [
+      "/projectImage/project1/friendconnect.png",
+      "/projectImage/project1/friendconnect2.png",
+      "/projectImage/project1/friendconnect3.png"
+    ],
+    "github": "https://github.com/2022BCD0052/Yogesh-Portfolio-1",  
+    "liveDemo": "https://yogesh-portfolio-1.vercel.app/"
   },
   {
-    title: "Crypto Trading Website",
-    name: "Crypto Trading project",
-    des: "We buit this website for BigMoney group",
-    des1: "This project was built to enable clients all over the world to buy and sell their crypto.",
-
-    images: ["/crypto5.png", "/crypto3.png", "/crypto1.png"],
+    "title": "FriendConnect Project",
+    "name": "FriendConnect ",
+    "des": "I developed this project in 2023 to guide students worldwide on how to build a portfolio using React.js and Next.js. This portfolio website allows developers to showcase their projects to clients or potential employers. The design was inspired by a friend’s concept and has been carefully crafted to be responsive across all devices.",
+    "des1": "This project was built using React.js, Next.js, NodeMailer, and other modern web technologies!",
+    "developer": "Yogesh Kumar Saini",
+    "techStack": ["React.js", "Next.js", "NodeMailer", "Tailwind CSS", "MongoDB"],
+    "images": [
+      "/projectImage/project1/friendconnect.png",
+      "/projectImage/project1/friendconnect2.png",
+      "/projectImage/project1/friendconnect3.png"
+    ],
+    "github": "https://github.com/2022BCD0052/Yogesh-Portfolio-1",  
+    "liveDemo": "https://yogesh-portfolio-1.vercel.app/"
   },
-  {
-    title: "Food Delivery Company WebSite",
-    name: "Yum Food Company",
-    des: "We did  this project for Yum Foods Located in USA.",
-    des1: "This website's graphic design is fantastic. Animated sliders are painstakingly built and created in WordPress with Revolution Slider. All of the product photos are photo manipulations created in Adobe Photoshop.",
-
-    images: ["/onboarding.jpg","/yumfood.jpg", "/deliveryguy.png"],
-  },
+  
 ];
--- src/app/page/portfolio-detail-design/index.js
+++ src/app/page/portfolio-detail-design/index.js
@@ -25,7 +25,7 @@ export default function Index({ id, data, DataArray }) {
     } else {
       setPrev(JSON.parse(id) - 1);
     }
-  },[DataArray.length, id]);
+  }, [DataArray.length, id]);
 
   return (
     <React.Fragment>
@@ -34,86 +34,105 @@ export default function Index({ id, data, DataArray }) {
         <Image
           src={data?.images[0]}
           alt="bg photo"
-          className="h-[400px]  lg:mt-24 sm:mt-0  object-fill"
+          className="h-[400px]  lg:mt-24  hover:grayscale sm:mt-0  object-fill"
           width={0}
           height={20}
           sizes="100vw"
-          style={{ width: '100%', backgroundSize: "cover" }} // optional
+          style={{ width: "100%", backgroundSize: "cover" }} // optional
         />
       </div>
 
-      <div className="absolute z-30 top-[42px] sm:top-[200px] sm:left-[65px] justify-center  items-center w-full px-10 sm:w-3/4 xl:w-1/2 sm:px-0  sm:text-left">
+      <div className="absolute z-30 lg:top top-[42px] sm:top-[200px] sm:left-[65px] justify-center items-center w-full px-10 sm:w-3/4 xl:w-1/2 sm:px-0 sm:text-left ">
         <div className="container m-auto">
           <div className="max-w-[650px] w-[100%] m-auto">
-            <p className="opacity-3 sm:text-left text-center font-sans text-[#223740] mt-20 lg:mt-10 md:-mt-10">
-              Project Sample
-            </p>
+            <p className="opacity-3 sm:text-left text-center font-sans text-[#223740] mt-20 lg:mt-10 md:-mt-10"></p>
             <h1 className="opacity-3 text-[#223740] sm:text-left text-center w-full sm:w-3/4 font-recoletaBold text-4xl md:text-4xl lg:text-4xl xl:text-4xl">
               {data?.title}
             </h1>
+
+            {/* GitHub & Live Demo Links */}
+            <div className="flex gap-4 mt-4 sm:mt-6 justify-center sm:justify-start">
+              {data?.github && (
+                <a
+                  href={data.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-[#223740] text-white px-6 py-3 rounded-lg hover:bg-[#48AFDE] transition-all duration-300"
+                >
+                  GitHub
+                </a>
+              )}
+              {data?.liveDemo && (
+                <a
+                  href={data.liveDemo}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-[#48AFDE] text-white px-6 py-3 rounded-lg hover:bg-[#223740] transition-all duration-300"
+                >
+                  Live Demo
+                </a>
+              )}
+            </div>
           </div>
         </div>
       </div>
 
       <div
-        className="grid  grid-cols-12  relative space-x-0 lg:space-x-8"
+        className="grid  grid-cols-12  relative space-x-0 lg:space-x-8  lg:pt-20 pb-20"
         style={{
           background:
             "linear-gradient(90deg, rgba(238, 247, 251, 1) 58%, rgba(255, 255, 255, 1) 52%",
         }}
       >
-        <div className="col-span-12  lg:col-span-8 mb-20 scrol lg:px-0 sm:px-20">
+        <div className="col-span-12  lg:col-span-8  scrol lg:px-0 sm:px-20">
           {data?.images.map((item) => (
-            <div key={item} className="flex justify-center lg:justify-end  items-center">
+            <div
+              key={item}
+              className="flex justify-center lg:justify-end px-2  items-center"
+            >
               <Image
                 src={item}
                 alt="portfolio photos"
                 height={100}
                 width={800}
-                className="mt-20 rounded-lg"
+                className="mt-2 rounded-lg hover:grayscale"
               />
             </div>
           ))}
         </div>
-        <div className="col-span-12  lg:col-span-4 lg:px-0 sm:px-20">
-          <div className="mt-10 sm:mt-24 w-full lg:max-w-[300px] lg:px-1  px-3 sticky top-36 pb-14">
-            <h1 className="text-3xl  mb-4 text-[#48AFDE]">{data?.name}</h1>
+
+        <div className="col-span-12 lg:col-span-4 lg:px-0 sm:px-20">
+          <div className="mt-10 sm:mt-18 w-full lg:max-w-[300px] lg:px-1 px-3 sticky top-36 pb-14">
+            <h1 className="text-3xl mb-4 text-[#48AFDE]">{data?.name}</h1>
             <p className="text-[14px] font-sans mb-4 text-[#223740]">
               {data?.des}
             </p>
-            <p id="highlight" className="my-2  text-dark text-[20px] font-sans">
+
+            <p id="highlight" className="my-2 text-dark text-[20px] font-sans">
               Project Descriptions
             </p>
             <p className="text-[14px] font-sans mb-4 text-[#223740]">
-              {" "}
               {data?.des1}
             </p>
 
+            <p className="text-[16px] font-semibold text-[#223740] mt-4 mb-2">
+              Tech Stack:
+            </p>
             <div className="flex flex-wrap">
-              <h1 className="mr-5 text-[14px]  bg-[#63c5f1] lg:bg-[#EEF7FB]  px-2 py-1  rounded-xl font-sans  mb-4 text-white lg:text-[#6A787D]">
-                UI/US Design
-              </h1>
-              <h1 className="mr-5 text-[14px]  bg-[#63c5f1] lg:bg-[#EEF7FB]  px-2 py-1  rounded-xl font-sans  mb-4 text-white lg:text-[#6A787D]">
-                Next.js
-              </h1>
-              <h1 className="mr-5 text-[14px]  bg-[#63c5f1] lg:bg-[#EEF7FB]  px-2 py-1  rounded-xl font-sans  mb-4 text-white lg:text-[#6A787D]">
-                React.js
-              </h1>
-              <h1 className="mr-5 text-[14px]  bg-[#63c5f1] lg:bg-[#EEF7FB]  px-2 py-1  rounded-xl font-sans  mb-4 text-white lg:text-[#6A787D]">
-                Node.js
-              </h1>
-              <h1 className="mr-5 text-[14px]  bg-[#63c5f1] lg:bg-[#EEF7FB]  px-2 py-1  rounded-xl font-sans  mb-4 text-white lg:text-[#6A787D]">
-                Express
-              </h1>
-              <h1 className="mr-5 text-[14px]  bg-[#63c5f1] lg:bg-[#EEF7FB]  px-2 py-1  rounded-xl font-sans  mb-4 text-white lg:text-[#6A787D]">
-                UI/US Design
-              </h1>
+              {data?.techStack?.map((tech, index) => (
+                <h1
+                  key={index}
+                  className="mr-3 text-[14px] bg-[#63c5f1] lg:bg-[#EEF7FB] px-2 py-1 rounded-xl font-sans mb-3 text-white lg:text-[#6A787D]"
+                >
+                  {tech}
+                </h1>
+              ))}
             </div>
           </div>
         </div>
       </div>
 
-      <div className="relative flex bg-accent-color h-48  text-white">
+      <div className="relative flex  bg-accent-color h-48  text-white">
         <div
           className={`group w-1/2 flex items-center justify-center bg-cover  ${hind.className}`}
           style={{ backgroundImage: `url(${DataArray[prev]?.images[0]})` }}
@@ -168,6 +187,8 @@ export default function Index({ id, data, DataArray }) {
           </a>
         </div>
       </div>
+
+
     </React.Fragment>
   );
 }
